81511a517f70426a39bb3fbd287baa93c56ca605
mario4l <mario.vargas29@gmail.com>
2020-09-25 16:10:26 -0700

    added logic to display lbs or kg to UI

--- app.js
+++ app.js
@@ -6,15 +6,19 @@
 function unitCheck() {
   var lbs = document.getElementById("lbs");
   var kg = document.getElementById("kg");
-
   var weight = document.getElementById("entered-weight");
+  var displayMessage = document.getElementById("display-message");
 
   if (lbs.checked) {
     console.log('Weight in Kg: ' + weight.value);
+        var message = 'Weight in Kg: ' + weight.value;
+    displayMessage.innerHTML = message;
   }
 
   if (kg.checked) {
     console.log('Weight in lbs: ' + weight.value);
+    var message = 'Weight in lbs: ' + weight.value;
+    displayMessage.innerHTML = message;
   }
 
 }
