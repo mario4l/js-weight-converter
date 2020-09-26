2a6c4f30abd02a250c877621bc66408b84272591
mario4l <mario.vargas29@gmail.com>
2020-09-25 17:00:35 -0700

    Added logic for weight conversion

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-// console.log('hello')
-
-// var x = document.getElementById('kg').checked = true;
-// console.log(x);
-
+// formula
+// kg / 0.45
+// lbs * 0.45
 function unitCheck() {
-  var lbs = document.getElementById("lbs");
-  var kg = document.getElementById("kg");
-  var weight = document.getElementById("entered-weight");
-  var displayMessage = document.getElementById("display-message");
+  var lbs = document.getElementById('lbs');
+  var kg = document.getElementById('kg');
+  var weight = document.getElementById('entered-weight');
+  var displayMessage = document.getElementById('display-message');
 
   if (lbs.checked) {
-    console.log('Weight in Kg: ' + weight.value);
-        var message = 'Weight in Kg: ' + weight.value;
+    // console.log('Weight in Kg: ' + weight.value);
+    var message = 'Weight in Kg: ' + weight.value * 0.45;
     displayMessage.innerHTML = message;
   }
 
   if (kg.checked) {
-    console.log('Weight in lbs: ' + weight.value);
-    var message = 'Weight in lbs: ' + weight.value;
+    // console.log('Weight in lbs: ' + weight.value);
+    var message = 'Weight in lbs: ' + weight.value / 0.45;
     displayMessage.innerHTML = message;
   }
 
   // TODO: add fall back to UI
-  if (!lbs.checked || kg.checked) {
-    console.log('Please select (L)lbs or (K)g');
-  }
-
+  // if (!lbs.checked || kg.checked) {
+  //   console.log('Please select (L)lbs or (K)g');
+  // }
 }
 
-document.getElementById("main-form").addEventListener("submit", function (e) {
+document.getElementById('main-form').addEventListener('submit', function (e) {
   e.preventDefault();
 
   unitCheck();
