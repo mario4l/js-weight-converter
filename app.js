e1428fe759b2bca8a77117677a07b1d8bfaa2e14
mario4l <mario.vargas29@gmail.com>
2020-09-25 17:46:13 -0700

    added fall back if unit is not selected

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ function unitCheck() {
   var weight = document.getElementById('entered-weight');
   var displayMessage = document.getElementById('display-message');
 
+  if (!lbs.checked && !kg.checked) {
+    var message = 'Please select (L)lbs or (K)g';
+    displayMessage.innerHTML = message;
+  }
+
   if (lbs.checked) {
     // console.log('Weight in Kg: ' + weight.value);
     var message = 'Weight in Kg: ' + weight.value * 0.45;
@@ -18,11 +23,6 @@ function unitCheck() {
     var message = 'Weight in lbs: ' + weight.value / 0.45;
     displayMessage.innerHTML = message;
   }
-
-  // TODO: add fall back to UI
-  // if (!lbs.checked || kg.checked) {
-  //   console.log('Please select (L)lbs or (K)g');
-  // }
 }
 
 document.getElementById('main-form').addEventListener('submit', function (e) {
